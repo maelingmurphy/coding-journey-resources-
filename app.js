98eb6afea7dfd3c2e850ab145835998a482ff4fe
Maeling <maelingmurphy@gmail.com>
2020-12-01 23:50:24 -0500

    Added jQuery functionality to be able to toggle between showing and hiding multiple divs

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
-function showContent() {
-    var div = document.getElementById("C-info");
-    if (div.style.display === "none") {
-        div.style.display = "block";
-    }
-    else {
-        div.style.display = "none";
-    }
-}
+$(document).ready(function() {
+    // all custom jQuery will go here
+    $("#C-button").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#C-info").slideUp();
+            $("#C-info").slideToggle("slow", function () {
+            });
+    });
+
+    $("#Python-button").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#Python-info").slideUp();
+            $("#Python-info").slideToggle("slow", function () {
+            });
+    });
+
+    $("#JavaScript-button").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#JavaScript-info").slideUp();
+            $("#JavaScript-info").slideToggle("slow", function () {
+            });
+    });
+});
