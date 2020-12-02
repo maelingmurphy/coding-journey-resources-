0a429f754a2fd4c8c9be9eb8cb143500ea9e3984
Maeling <maelingmurphy@gmail.com>
2020-12-01 20:35:29 -0500

    Added additional html page, JS interactivity, Bootstrap CSS Framework

---
+++ app.js
@@ -0,0 +1,9 @@
+function showContent() {
+    var div = document.getElementById("C-info");
+    if (div.style.display === "none") {
+        div.style.display = "block";
+    }
+    else {
+        div.style.display = "none";
+    }
+}
