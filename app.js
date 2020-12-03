8e05262edbfcc7266dd4e7f0209c78d128076e99
Maeling <maelingmurphy@gmail.com>
2020-12-02 19:05:59 -0500

    Fixed jQuery navbar interactivity

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 $(document).ready(function() {
-    // all custom jQuery will go here
+    // Navbar interactivity - probably a way to simplify this by writing a function
+    // to pass into click()
+    $("#C-dropdown").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#C-info").slideUp();
+            $("#C-info").slideToggle("slow", function () {
+            });
+    });
+
+    $("#Python-dropdown").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#Python-info").slideUp();
+            $("#Python-info").slideToggle("slow", function () {
+            });
+    });
+
+    $("#JavaScript-dropdown").click(function(e) {
+        e.preventDefault();
+            $(".options-info").not("#JavaScript-info").slideUp();
+            $("#JavaScript-info").slideToggle("slow", function () {
+            });
+    });
+    
+    // Toggle Show/Hide for Multiple Divs
     $("#C-button").click(function(e) {
         e.preventDefault();
             $(".options-info").not("#C-info").slideUp();
