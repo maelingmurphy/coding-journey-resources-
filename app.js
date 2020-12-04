63917112f6c043d3c5fa1e9bf5d50a90b4c2191b
Maeling <maelingmurphy@gmail.com>
2020-12-03 19:34:18 -0500

    Made major updates to CSS styling and made a few structural changes in HTML

--- app.js
+++ app.js
@@ -1,25 +1,6 @@
 $(document).ready(function() {
-    // Navbar interactivity - probably a way to simplify this by writing a function
-    // to pass into click()
-    $("#C-dropdown").click(function(e) {
-        e.preventDefault();
-            $(".options-info").not("#C-info").hide();
-            $("#C-info").show();
-    });
     
-    $("#Python-dropdown").click(function(e) {
-        e.preventDefault();
-            $(".options-info").not("#Python-info").hide();
-            $("#Python-info").show();
-    });
-
-    $("#JavaScript-dropdown").click(function(e) {
-        e.preventDefault();
-            $(".options-info").not("#JavaScript-info").hide();
-            $("#JavaScript-info").show();
-    });
-    
-    // Toggle Show/Hide for Multiple Divs
+    // Toggle Show/Hide for Multiple Divs on languages.html page
     $("#C-button").click(function(e) {
         e.preventDefault();
             $(".options-info").not("#C-info").hide();
